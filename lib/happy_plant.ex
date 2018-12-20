bc3e62ba2ea0f5a71b61ff1296de0c5f39e628f0
Greg Fisher <me@gnfisher.com>
2018-12-20 14:09:40 -0300

    Need to explore Date/Time in elixir..

--- lib/happy_plant.ex
+++ lib/happy_plant.ex
@@ -1,9 +1,13 @@
 defmodule HappyPlant do
   defmodule Plant do
-    defstruct height: 0, health: 3
+    defstruct height: 0, health: 3, last_watered: DateTime.utc_now
 
     def create() do
       %HappyPlant.Plant{}
+    end
+
+    def create_at(timestamp) do
+      %HappyPlant.Plant{last_watered: timestamp}
     end
   end
 end
--- test/happy_plant_test.exs
+++ test/happy_plant_test.exs
@@ -2,7 +2,21 @@ defmodule HappyPlantTest do
   use ExUnit.Case
   doctest HappyPlant
 
-  test "Starts life with 3 health and 0 height" do
+  test "create/0 creates a new plant with health 3, height 0" do
     assert HappyPlant.Plant.create == %HappyPlant.Plant{health: 3, height: 0}
+  end
+
+  test "create_at/1 creates a new plant with last watered equal to its argument" do
+    timestamp = DateTime.utc_now
+
+    result = HappyPlant.Plant.create_at timestamp
+
+    assert result.last_watered == timestamp
+  end
+
+  @tag :pending
+  test "water/2 creates a new plant with +1 health if last_watered is between 10 and 30 seconds ago" do
+    # TODO
+    assert false
   end
 end
